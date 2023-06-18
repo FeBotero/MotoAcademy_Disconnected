8dafbefc57bb2937d4e5130f35dd003ae1981627
FeBotero <frpbotero@gmail.com>
2023-06-18 19:16:44 -0400

    Create function to next step

--- app/src/main/java/com/motoacademy/disconnected/MainActivity.java
+++ app/src/main/java/com/motoacademy/disconnected/MainActivity.java
@@ -3,6 +3,11 @@ package com.motoacademy.disconnected;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,34 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView userText = findViewById(R.id.inputLogin);
+        Button butCreate = findViewById(R.id.buttonCreate);
+        Button butNext = findViewById(R.id.buttonNext);
 
+        butCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              String userInput = userText.getText().toString();
+
+              if(TextUtils.isEmpty(userInput)){
+                  Toast.makeText(MainActivity.this,"O campo está vazio!", Toast.LENGTH_SHORT).show();
+              }else{
+                  Toast.makeText(MainActivity.this,"Seguinte!", Toast.LENGTH_SHORT).show();
+              }
+            }
+        });
+
+        butNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userInput = userText.getText().toString();
+
+                if(TextUtils.isEmpty(userInput)){
+                    Toast.makeText(MainActivity.this,"O campo está vazio!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Seguinte!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
